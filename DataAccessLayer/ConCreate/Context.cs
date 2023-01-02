43dd9dfae0ff50f6a0c8a39f54143c44be6c2d5f
AhmetMel <ahmet.melih0056@gmail.com>
2023-01-02 22:05:56 +0300

    Context güncellendi

--- DataAccessLayer/ConCreate/Context.cs
+++ DataAccessLayer/ConCreate/Context.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using EntityLayer;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,22 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.ConCreate
 {
-    public class Context
+    public class Context:DbContext
     {
-    }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) 
+        {
+            optionsBuilder.UseLazyLoadingProxies().UseSqlServer("server=LAPTOP-GC0KUGMH\\SQLEXPRESS; ; database=DBBusBilet ;Encrypt=False; Integrated Security=True;");
+        }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<CargoProcess> CargoProcesses { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customization> Customizations { get; set; }
+        public DbSet<Favorite> Favorites { get; set; }
+        public DbSet<Menu> Menus { get; set; }
+        public DbSet<Option> Options { get; set; }
+        public DbSet<OptionType> OptionTypes { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCustomization> ProductCustomizations { get; set; }
+	}
 }
