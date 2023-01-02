a1a567df096561e57bcd6b1baba5ad3e6a639b80
beyzaekincioglu <beyzaekincioglu1@gmail.com>
2023-01-02 23:15:17 +0300

    IGenericDal eklendi

---
+++ DataAccessLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IGenericDal<T>
+    {
+        void insert(T entity);  
+        void delete(T entity);  
+        void update(T entity);  
+        List<T> list();
+        List<T> listBy(Expression<Func<T,bool>>filter);
+       T get(Expression<Func<T,bool>>filter);
+    }
+}
