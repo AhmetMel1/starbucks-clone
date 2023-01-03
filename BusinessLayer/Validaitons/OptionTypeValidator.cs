8318b1e2fc4d8708584364bfc988a3f9b143c1fd
AhmetMel <ahmet.melih0056@gmail.com>
2023-01-03 18:45:24 +0300

    Validator eklendi

---
+++ BusinessLayer/Validaitons/OptionTypeValidator.cs
@@ -0,0 +1,21 @@
+﻿using EntityLayer;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Validaitons
+{
+    public class OptionTypeValidator:AbstractValidator<OptionType>
+    {
+        public OptionTypeValidator() 
+        {
+            //Rule for OptionName
+            RuleFor(optionType => optionType.optionTypeName).NotEmpty().WithMessage("Boş geçilemez");
+            RuleFor(optionType => optionType.optionTypeName).MaximumLength(50).WithMessage("Maximum 50 karakter girilebilir.");
+            RuleFor(optionType => optionType.optionTypeName).MinimumLength(2).WithMessage("Minimum 2 karakter girilmedilir.");
+        }    
+    }
+}
---
+++ BusinessLayer/Validaitons/OptionValidator.cs
@@ -0,0 +1,23 @@
+﻿using EntityLayer;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Validaitons
+{
+    public class OptionValidator:AbstractValidator<Option>
+    {
+        public OptionValidator() 
+        {
+            //Rule for OptionName
+            RuleFor(option => option.optionName).NotEmpty().WithMessage("Boş geçilemez");
+            RuleFor(option => option.optionName).MaximumLength(50).WithMessage("Maximum 50 karakter girilebilir.");
+            RuleFor(option => option.optionName).MinimumLength(2).WithMessage("Minimum 2 karakter girilmedilir.");
+            //Rule for OptionUnitPrice
+            RuleFor(option => option.optionUnitPrice).NotEmpty().WithMessage("Boş geçilemez");
+        }
+    }
+}
