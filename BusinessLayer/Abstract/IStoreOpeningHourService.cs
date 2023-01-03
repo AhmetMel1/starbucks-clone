c87a389c3a750b41bba55a78cad122d117cfe2cc
kubracl1k <kbr.kocer54@gmail.com>
2023-01-03 16:17:21 +0300

    BusinessLayer interface eklendi

---
+++ BusinessLayer/Abstract/IStoreOpeningHourService.cs
@@ -0,0 +1,19 @@
+﻿using EntityLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IStoreOpeningHourService
+    {
+        void StoreOpeningHourInsert(StoreOpeningHour storeOpeningHour);
+        void StoreOpeningHourUpdate(StoreOpeningHour storeOpeningHour);
+        void StoreOpeningHourDelete(StoreOpeningHour storeOpeningHour);
+        List<StoreOpeningHour> StoreOpeningHourList();
+        StoreOpeningHour StoreOpeningHourGetById(int id);
+
+    }
+}
---
+++ BusinessLayer/Abstract/IStoreService.cs
@@ -0,0 +1,18 @@
+﻿using EntityLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IStoreService
+    {
+        void StoreInsert(Store store);
+        void StoreUpdate(Store store);
+        void StoreDelete(Store store);
+        List<Store> storeList();
+        Store StorestoreGetById(int id);
+    }
+}
