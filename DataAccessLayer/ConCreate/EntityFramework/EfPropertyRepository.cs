f9aec8c7acbfd569ebba13d8c5c1bf1badef1759
kubracl1k <kbr.kocer54@gmail.com>
2023-01-03 12:00:29 +0300

    entityframeworkte Repository eklendi

---
+++ DataAccessLayer/ConCreate/EntityFramework/EfPropertyRepository.cs
@@ -0,0 +1,15 @@
+﻿using DataAccessLayer.Abstract;
+using EntityLayer;
+using Microsoft.EntityFrameworkCore.Metadata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.ConCreate.EntityFramework
+{
+  public class EfPropertyRepository:GenericRepository<Property>,IPropertyDal
+    {
+    }
+}
