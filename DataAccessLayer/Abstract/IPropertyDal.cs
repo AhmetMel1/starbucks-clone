1018350ffdf1c10ddc1b46c04b68fa937dcd8ae0
kubracl1k <kbr.kocer54@gmail.com>
2023-01-03 11:17:28 +0300

    interface eklendi

---
+++ DataAccessLayer/Abstract/IPropertyDal.cs
@@ -0,0 +1,13 @@
+﻿using EntityLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    internal interface IPropertyDal:IGenericDal<Property>
+    {
+    }
+}
