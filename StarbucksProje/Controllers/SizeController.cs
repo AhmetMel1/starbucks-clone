2a4f7f6f10a786b9a17b67f8fb955bddc5b4496a
AhmetMel <ahmet.melih0056@gmail.com>
2023-01-05 12:50:41 +0300

    Size işlemleri yapıldı

--- StarbucksProje/Controllers/SizeController.cs
+++ StarbucksProje/Controllers/SizeController.cs
@@ -1,12 +1,49 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using DataAccessLayer.ConCreate.EntityFramework;
+using EntityLayer;
 
 namespace StarbucksProje.Controllers
 {
 	public class SizeController : Controller
 	{
+		SizeManager sm = new SizeManager(new EfSizeRepository());
 		public IActionResult Index()
 		{
-			return View();
+			var sizes = sm.sizeList();
+			return View(sizes);
 		}
-	}
+        [HttpGet]
+        public IActionResult AddSize()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddSize(Size size)
+        {
+            sm.sizeInsert(size);
+            return RedirectToAction("Index");
+        }
+        public IActionResult DeleteSize(int id)
+        {
+            Size size = sm.sizeGetById(id);
+            size.sizeDeleted = true;
+            sm.sizeUpdate(size);
+            return RedirectToAction("Index");
+        }
+        public IActionResult UpdateSize(int id)
+        {
+            Size size = sm.sizeGetById(id);
+         
+            return View(size);
+        }
+        [HttpPost]
+        public IActionResult UpdateSize(Size size)
+        {
+            
+                sm.sizeUpdate(size);
+                return RedirectToAction("Index");           
+        }
+    }
 }
