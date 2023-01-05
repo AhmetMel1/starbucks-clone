2e790ec3a4b6604ce13db3debb8edd50b089ecf7
kubracl1k <kbr.kocer54@gmail.com>
2023-01-05 12:59:45 +0300

    validator eklendi,user entitylayer birthday değiştirildi

---
+++ BusinessLayer/Validaitons/StoreProductValidator.cs
@@ -0,0 +1,15 @@
+﻿using EntityLayer;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Validaitons
+{
+    public class StoreProductValidator:AbstractValidator<StoreProduct>
+    {
+        public StoreProductValidator() { }
+    }
+}
