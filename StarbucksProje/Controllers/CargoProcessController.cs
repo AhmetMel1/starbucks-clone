6b1ceff554c3297d93c8956bead8a1a812b81445
beyzaekincioglu <beyzaekincioglu1@gmail.com>
2023-01-05 15:11:28 +0300

    Tablo eklendi

--- StarbucksProje/Controllers/CargoProcessController.cs
+++ StarbucksProje/Controllers/CargoProcessController.cs
@@ -16,5 +16,6 @@ namespace StarbucksProje.Controllers
         {
             return View();
         }
+
     }
 }
---
+++ StarbucksProje/Controllers/FavoriteController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StarbucksProje.Controllers
+{
+    public class FavoriteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
