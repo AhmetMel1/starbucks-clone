9989de415bc99aaeeddab4bec5328c1185b119d1
AhmetMel <ahmet.melih0056@gmail.com>
2023-01-07 02:19:17 +0300

    Product Size tablosu eklendi.

---
+++ StarbucksProje/Controllers/ProductSizeController.cs
@@ -0,0 +1,57 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.ConCreate.EntityFramework;
+using EntityLayer;
+using Microsoft.AspNetCore.Mvc;
+using StarbucksProje.Models;
+
+namespace StarbucksProje.Controllers
+{
+    public class ProductSizeController : Controller
+    {
+        ProductSizeManager psm=new ProductSizeManager(new EfProductSizeRepository());
+        ProductManager pm=new ProductManager(new EfProductRepository());
+        SizeManager sm=new SizeManager(new EfSizeRepository());
+        public IActionResult Index()
+        {
+            var pruductSizes=psm.productSizeList();
+            return View(pruductSizes);
+        }
+        [HttpGet]
+        public IActionResult AddProductSize()
+        {
+            ProductSizeModel model=new ProductSizeModel();
+            model.productModel=pm.productList();
+            model.sizeModel = sm.sizeList();
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult AddProductSize(ProductSize productSize)
+        {
+            psm.productSizeUpdate(productSize);
+            return RedirectToAction("Index");
+        }
+        public IActionResult DeleteProductSize(int id)
+        {
+            ProductSize productSize = psm.productSizeGetById(id);
+            productSize.productSizeDeleted = true;
+            psm.productSizeUpdate(productSize);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult UpdateProductSize(int id)
+        {
+            ProductSizeModel model = new ProductSizeModel();
+            model.productModel = pm.productList();
+            model.sizeModel = sm.sizeList();
+            model.productSizeModel=psm.productSizeGetById(id);
+            return View(model);
+        }
+        [HttpPost]
+        public IActionResult UpdateProductSize(ProductSize productSize)
+        {
+            psm.productSizeUpdate(productSize);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ StarbucksProje/Models/ProductSizeModel.cs
@@ -0,0 +1,11 @@
+﻿using EntityLayer;
+
+namespace StarbucksProje.Models
+{
+    public class ProductSizeModel
+    {
+        public ProductSize productSizeModel { get; set; }
+        public IEnumerable<Product> productModel { get; set; }
+        public IEnumerable<Size> sizeModel { get; set; }
+    }
+}
