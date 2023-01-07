0a01b1e27eec1674cdb794dfa37d6ce5770dbf44
AhmetMel <ahmet.melih0056@gmail.com>
2023-01-08 00:22:44 +0300

    Hatalar düzeltildi

---
+++ StarbucksProje/Models/OptionTypeModel.cs
@@ -0,0 +1,10 @@
+﻿using EntityLayer;
+
+namespace StarbucksProje.Models
+{
+    public class OptionTypeModel
+    {
+        public List<Option> optionModel { get; set; }
+        public IEnumerable<OptionType> optionTypeModel { get; set; }
+    }
+}
