0ce10bc8ff3a4dfcee5d164acc12ba28f82c69b8
kubracl1k <kbr.kocer54@gmail.com>
2023-01-08 00:44:08 +0300

    worktime controller eklendi

---
+++ StarbucksProje/Controllers/WorkTimeController.cs
@@ -0,0 +1,50 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.ConCreate.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using EntityLayer;
+
+namespace StarbucksProje.Controllers
+{
+    public class WorkTimeController : Controller
+    {
+        WorkTimeManager wm = new WorkTimeManager(new EfWorkTimeRepository());
+
+        public IActionResult Index()
+        {
+            var WorkTime = wm.WorkTimeList();
+            return View();
+        }
+        [HttpGet]
+        public IActionResult AddWorkTime()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddWorkTime(WorkTime workTime)
+        {
+            wm.WorkTimeInsert(workTime);
+                return RedirectToAction("Index");
+        }
+        public IActionResult DeleteWorkTime(int id)
+        {
+            WorkTime workTime = wm.WorkTimeGetById(id);
+            workTime.WorkTimeDeleted = true;
+            wm.WorkTimeUpdate(workTime);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult UpdateWorkTime(int id)
+        {
+            WorkTime workTime = wm.WorkTimeGetById(id);
+            return View(workTime);
+
+        }
+        [HttpPost]
+        public IActionResult UpdateWorkTime(WorkTime workTime)
+        {
+            wm.WorkTimeUpdate(workTime);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
