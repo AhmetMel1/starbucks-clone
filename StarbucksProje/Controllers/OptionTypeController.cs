1ef12abcecbe15b655927908b6f3eb76673786c2
AhmetMel <ahmet.melih0056@gmail.com>
2023-01-08 00:22:10 +0300

    Option Type eklendi

---
+++ StarbucksProje/Controllers/OptionTypeController.cs
@@ -0,0 +1,49 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.ConCreate.EntityFramework;
+using EntityLayer;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StarbucksProje.Controllers
+{
+    public class OptionTypeController : Controller
+    {
+        OptionTypeManager otm = new OptionTypeManager(new EfOptionTypeRepository());
+        public IActionResult Index()
+        {
+            var optionTypes = otm.optionTypeList();
+            return View(optionTypes);
+        }
+        [HttpGet]
+        public IActionResult AddOptionType()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddOptionType(OptionType optionType)
+        {
+            otm.optionTypeInsert(optionType);
+            return RedirectToAction("Index");
+        }
+        public IActionResult DeleteOptionType(int id)
+        {
+            OptionType optionType = otm.optionTypeGetById(id);
+            optionType.optionTypeDeleted = true;
+            otm.optionTypeUpdate(optionType);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult UpdateOptionType(int id)
+        {
+            OptionType optionType = otm.optionTypeGetById(id);
+            return View(optionType);
+        }
+        [HttpPost]
+        public IActionResult UpdateOptionType(OptionType optionType)
+        {
+
+            otm.optionTypeUpdate(optionType);
+            return RedirectToAction("Index");
+        }
+    }
+}
