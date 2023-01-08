35ac43908e4610e9abe4c1396dddd8cc7e53a5ac
kubracl1k <kbr.kocer54@gmail.com>
2023-01-08 22:16:47 +0300

    storeproduct model ve controller eklendi

---
+++ StarbucksProje/Controllers/StoreProductController.cs
@@ -0,0 +1,57 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.ConCreate.EntityFramework;
+using EntityLayer;
+using Microsoft.AspNetCore.Mvc;
+using StarbucksProje.Models;
+
+namespace StarbucksProje.Controllers
+{
+    public class StoreProductController : Controller
+    {
+        StoreProductManager spm = new StoreProductManager(new EfStoreProductRepository());
+        StoreManager sm=new StoreManager(new EfStoreRepository());
+        ProductManager pm=new ProductManager(new EfProductRepository());
+
+        public IActionResult Index()
+        {
+            var storeProduct = spm.StoreProductsList();
+            return View(storeProduct);
+        }
+        [HttpGet]
+        public IActionResult AddStoreProduct()
+        {
+            StoreProductModel model = new StoreProductModel();
+            model.storeModel = sm.storeList();
+            model.productModel=pm.productList();
+            return View(model);
+        }
+        [HttpPost]
+        public IActionResult AddStoreProduct(StoreProduct storeProduct)
+        {
+            spm.StoreProductUpdate(storeProduct);
+            return RedirectToAction("Index");
+        }
+        public IActionResult DeleteStoreProduct(int id)
+        {
+            StoreProduct storeProduct=spm.StoreProductGetById(id);
+            storeProduct.StoreProductDeleted=true;
+            spm.StoreProductUpdate(storeProduct);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult UpdateStoreProduct(int id)
+        {
+            StoreProductModel model=new StoreProductModel();
+            model.storeModel = sm.storeList();
+            model.productModel=pm.productList();
+            model.storeProductModel = spm.StoreProductGetById(id);
+            return View(model);
+        }
+        [HttpPost]
+        public IActionResult UpdateStoreProduct(StoreProduct storeProduct)
+        {
+            spm.StoreProductUpdate(storeProduct);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ StarbucksProje/Models/StoreProductModel.cs
@@ -0,0 +1,11 @@
+﻿using EntityLayer;
+
+namespace StarbucksProje.Models
+{
+    public class StoreProductModel
+    {
+        public StoreProduct storeProductModel { get; set; }
+        public IEnumerable<Product> productModel { get; set; }
+        public IEnumerable<Store> storeModel { get; set; }
+    }
+}
