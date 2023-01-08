0a20fc70f343437b6fb5ceb69e565f6d1595ab47
kubracl1k <kbr.kocer54@gmail.com>
2023-01-08 21:22:23 +0300

    StoreOpeningHour controller ve model eklendi

---
+++ StarbucksProje/Controllers/StoreOpeningHourController.cs
@@ -0,0 +1,56 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.ConCreate.EntityFramework;
+using EntityLayer;
+using Microsoft.AspNetCore.Mvc;
+using StarbucksProje.Models;
+
+namespace StarbucksProje.Controllers
+{
+    public class StoreOpeningHourController : Controller
+    {
+        StoreOpeningHourManager shm = new StoreOpeningHourManager(new EfStoreOpeningHourRepository());
+        WorkTimeManager wtm = new WorkTimeManager(new EfWorkTimeRepository());
+        StoreManager sm = new StoreManager(new EfStoreRepository());
+        public IActionResult Index()
+        {
+            var storeOpeningHour = shm.StoreOpeningHourList();
+            return View(storeOpeningHour);
+        }
+        [HttpGet]
+        public IActionResult AddStoreOpeningHour()
+        {
+            StoreOpeningHourModel model = new StoreOpeningHourModel();
+            model.workTimeModel = wtm.WorkTimeList();
+            model.storeModel = sm.storeList();
+            return View(model);
+        }
+        [HttpPost]
+        public IActionResult AddStoreOpeningHour(StoreOpeningHour storeOpeningHour)
+        {
+            shm.StoreOpeningHourInsert(storeOpeningHour);
+            return RedirectToAction("Index");
+        }
+        public IActionResult DeletetoreOpeningHour(int id)
+        {
+            var storeOpeningHour = shm.StoreOpeningHourGetById(id);
+            storeOpeningHour.StoreOpeningHourDeleted = true;
+            shm.StoreOpeningHourUpdate(storeOpeningHour);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult UpdateStoreOpeningHour(int id)
+        {
+            StoreOpeningHourModel model = new StoreOpeningHourModel();
+            model.workTimeModel = wtm.WorkTimeList();
+            model.storeModel = sm.storeList();
+            model.storeOpeningHourModel = shm.StoreOpeningHourGetById(id);
+            return View(model);
+        }
+        [HttpPost]
+        public IActionResult UpdateStoreOpeningHour(StoreOpeningHour storeOpeningHour)
+        {
+            shm.StoreOpeningHourUpdate(storeOpeningHour);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ StarbucksProje/Models/StoreOpeningHourModel.cs
@@ -0,0 +1,13 @@
+﻿using EntityLayer;
+
+namespace StarbucksProje.Models
+{
+    public class StoreOpeningHourModel
+    {
+        public StoreOpeningHour storeOpeningHourModel { get; set; }
+        public IEnumerable<WorkTime> workTimeModel { get; set; }
+        public IEnumerable<Store> storeModel { get; set; }
+
+
+    }
+}
