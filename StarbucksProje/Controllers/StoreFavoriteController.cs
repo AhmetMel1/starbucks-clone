e46211cbbc30c88339a887b042eb03ea1e310df7
kubracl1k <kbr.kocer54@gmail.com>
2023-01-08 20:50:35 +0300

    store favorite model ve controller eklendi

---
+++ StarbucksProje/Controllers/StoreFavoriteController.cs
@@ -0,0 +1,58 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.ConCreate.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using EntityLayer;
+using StarbucksProje.Controllers;
+using StarbucksProje.Models;
+
+namespace StarbucksProje.Controllers
+{
+    public class StoreFavoriteController : Controller
+    {
+        StoreFavoriteManager sfm = new StoreFavoriteManager(new EfStoreFavoriteRepository());
+        StoreManager sm=new StoreManager(new EfStoreRepository());
+        UserManager um=new UserManager(new EfUserRepository());
+        public IActionResult Index()
+        {
+            var storeFavorite = sfm.StoreFavoriteList();
+            return View(storeFavorite);
+        }
+        [HttpGet]
+        public IActionResult AddStoreFavorite() 
+        {
+            StoreFavoriteModel model = new StoreFavoriteModel();
+            model.storeModel = sm.storeList();
+            model.userModel=um.userList();
+            return View(model); 
+        }
+        [HttpPost]
+        public IActionResult AddStoreFavorite(StoreFavorite storeFavorite)
+        {
+            sfm.StoreFavoriteInsert(storeFavorite);
+            return RedirectToAction("Index");
+        }
+        public IActionResult DeleteStoreFavorite(int id)
+        {
+            var storeFavorite=sfm.StoreFavoriteGetById(id);
+            storeFavorite.StoreFavoriteDeleted=true;
+            sfm.StoreFavoriteUpdate(storeFavorite);
+            return RedirectToAction("Index");
+
+        }
+        [HttpGet]
+        public IActionResult UpdateStoreFavorite(int id)
+        {
+            StoreFavoriteModel model =new StoreFavoriteModel();
+            model.storeModel = sm.storeList();
+            model.userModel=um.userList();
+            model.storeFavoriteModel = sfm.StoreFavoriteGetById(id);
+            return View(model);
+        }
+        [HttpPost]
+        public IActionResult UpdateStoreFavorite(StoreFavorite storeFavorite)
+        {
+            sfm.StoreFavoriteUpdate(storeFavorite);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ StarbucksProje/Models/StoreFavoriteModel.cs
@@ -0,0 +1,11 @@
+﻿using EntityLayer;
+
+namespace StarbucksProje.Models
+{
+    public class StoreFavoriteModel
+    {
+        public StoreFavorite storeFavoriteModel { get; set; }
+        public IEnumerable<Store> storeModel { get; set; }
+        public IEnumerable<User> userModel { get; set; }
+    }
+}
