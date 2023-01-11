4ce9a88a59dd2159c2da5986b2d8bd46bdb58aec
AhmetMel <ahmet.melih0056@gmail.com>
2023-01-11 19:15:06 +0300

    Error Page eklendi

---
+++ StarbucksProje/Controllers/ErrorPageController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StarbucksProje.Controllers
+{
+	public class ErrorPageController : Controller
+	{
+		public IActionResult Errors(int code)
+		{
+			ViewBag.Code = code;
+			return View();
+		}
+	}
+}
--- StarbucksProje/Program.cs
+++ StarbucksProje/Program.cs
@@ -13,6 +13,8 @@ if (!app.Environment.IsDevelopment())
 	app.UseHsts();
 }
 
+app.UseStatusCodePagesWithReExecute("/ErrorPage/Errors","?code={0}");	
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
