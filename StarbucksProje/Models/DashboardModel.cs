a8921687d10830c60384e8ae6491550f3a72d20e
AhmetMel <ahmet.melih0056@gmail.com>
2023-01-31 04:47:39 +0300

    Düzenlemeler yapıldı.

---
+++ StarbucksProje/Models/DashboardModel.cs
@@ -0,0 +1,11 @@
+﻿using EntityLayer;
+
+namespace StarbucksProje.Models
+{
+    public class DashboardModel
+    {
+        public IEnumerable<Admin> adminModel { get; set; }
+        public IEnumerable<User> userModel { get; set; }
+        public IEnumerable<Order> orderModel { get; set; }
+    }
+}
