4dbae83993306fa225f6ccfb33ce340fd5f86819
beyzaekincioglu <beyzaekincioglu1@gmail.com>
2023-02-05 00:25:21 +0300

    slider eklendi

---
+++ BusinessLayer/Concrete/SliderManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class SliderManager : ISliderService
+    {
+         ISliderDal sliderDal;
+        public SliderManager(ISliderDal sliderDal)
+        {
+            this.sliderDal = sliderDal;
+        }
+        public void sliderDelete(Slider slider)
+        {
+            sliderDal.delete(slider);
+
+        }
+
+        public Slider SliderGetById(int id)
+        {
+            return sliderDal.get(x => x.sliderId == id);
+        }
+
+        public void sliderInsert(Slider slider)
+        {
+            sliderDal.insert(slider);
+        }
+
+        public List<Slider> sliderlist()
+        {
+            return sliderDal.list();
+        }
+
+        public void sliderUpdate(Slider slider)
+        {
+            sliderDal.update(slider);
+        }
+    }
+}
---
+++ EntityLayer/Slider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer
+{
+    public class Slider
+    {
+        [Key]
+        public int sliderId { get; set; }
+        [StringLength(50)]
+        public string sliderName { get; set; }
+        [StringLength(50)]
+        public string sliderInformation { get; set; }
+        [StringLength(50)]
+        public string sliderImage { get; set; }
+        public bool sliderDeleted { get; set; }
+
+    }
+}
